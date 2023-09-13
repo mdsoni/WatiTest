80d5aab1bbc7eff5af23855d85a558c5226edbf9
Manu Dev Soni <manu.dev.soni@gmail.com>
2023-09-13 16:18:23 +0530

    initial

---
+++ Controllers/AddController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WatiTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddController : Controller
+    {
+        [HttpPost]
+        public IActionResult Add([FromQuery]int num1, int num2)
+        {
+            return Ok(num1 + num2);
+        }
+    }
+}
