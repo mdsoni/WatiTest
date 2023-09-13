0e06d76119bb4d159f40d30ea47df0acf873ae91
Manu Dev Soni <manu.dev.soni@gmail.com>
2023-09-13 16:32:06 +0530

    Test case added

---
+++ CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace WatiTest
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase(2,3,5)]
+        [TestCase(0,0,0)]
+        [TestCase(2, 3, 4)]
+        [TestCase(10, -5, 5)]
+        public void TestAddition(int num1, int num2, int expectedSum)
+        {
+            int result = num1 + num2;
+            Assert.AreEqual(expectedSum, result);
+        }
+    }
+}
